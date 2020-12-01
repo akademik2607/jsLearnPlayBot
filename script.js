234d3eeac620968e4474392a2de16e4861958584
akademik2607 <akademik2607@mail.ru>
2020-12-01 22:38:42 +0300

    Исправил условие

--- script.js
+++ script.js
@@ -8,11 +8,12 @@ const play = function(){
     const result = Math.floor((Math.random() * 100) + 1);
     const answer = prompt("Угадайте число от 1 до 100");
     const action = function(answer){
-        if(!isNumber(answer) || answer === ''){
+
+        if(answer === null){alert("Игра окончена!");}
+        else if(!isNumber(answer) || answer === ''){
             answer = prompt("Введите число");
             action(answer);
         }
-        else if(answer === null){alert("Игра окончена!");}
         else if(+answer < result){
             answer = prompt("Загаданное число больше");
             action(answer);
